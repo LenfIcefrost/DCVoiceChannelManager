18636c8b8bb8ef6768792656a4490de08300cc92
LenfIcefrost <david77988@gmail.com>
2024-03-28 09:46:28 +0800

    feat: static data

---
+++ static_data.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+
+@dataclass()
+class bot_setting:
+    bot_token = ""
+
+    def to_save_data(self) -> str:
+        return f""
+
+@dataclass()
+class administrator:
+    users_id = [
+        
+    ]
+
+    def to_save_data(self) -> str:
+        return f""
